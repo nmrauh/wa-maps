015e3f7bee8469506274fd0f082467eddcc5216c
nmrauh <nmrauh@gmail.com>
2021-11-23 15:11:23 -0800

    tileset fixes

---
+++ tilesets/waterfall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="waterfall" tilewidth="32" tileheight="32" tilecount="576" columns="32">
+ <image source="waterfall.png" width="1024" height="576"/>
+</tileset>
