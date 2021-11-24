b52f5d38c99d94c8a7d053fda4b55401dab8824d
nmrauh <nmrauh@gmail.com>
2021-11-23 16:01:23 -0800

    remove tsx files

--- tilesets/dirt.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="dirt" tilewidth="32" tileheight="32" tilecount="336" columns="8">
- <image source="dirt.png" width="256" height="1344"/>
-</tileset>
--- tilesets/flower.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="flower" tilewidth="32" tileheight="32" tilecount="96" columns="8">
- <image source="flower.png" width="256" height="384"/>
-</tileset>
